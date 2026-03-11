c0a85d0267a486399ece61bedead824092c36a20
idioticidiot98-cell <idioticidiot98@gmail.com>
2026-03-11 19:53:05 +0000

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -1,4 +1,4 @@
---why does it not have animations? i removed them because horizon lc would use the default r6 animations while reanimated go to line 35 to replace the number with any hat half of the hats dont work tho
+--why does it not have animations? i removed them because horizon lc would use the default r6 animations while reanimated go to line 35 to replace the number with any hat and go to horizonlc.lua for more control tips
 local Global = (getgenv and getgenv()) or shared
 if not isfile and isfolder and writefile and (getsynasset or getcustomasset) then
     warn("Exploit will not run custom songs")
